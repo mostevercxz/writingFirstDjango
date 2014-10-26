92d38ff9c6850c60d811832642dbec9f85f510b5
purchaseDream <849069196@qq.com>
2014-10-26 06:27:55 -0700

    revision:13 go on learing part 3

--- firstDjangoApp/polls/urls.py
+++ firstDjangoApp/polls/urls.py
@@ -3,4 +3,7 @@ from polls import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
+	url(r'^(?P<qID>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<qID>\d+)/results/$', views.results, name='results'),
+	url(r'^(?P<qID>\d+)/vote/$', views.vote, name='vote'),
 )
--- firstDjangoApp/polls/views.py
+++ firstDjangoApp/polls/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(abc):
     return HttpResponse("this is my first view!")
+
+def detail(request, qID):
+	return HttpResponse("This is the %s question." % qID)
+
+def results(request, qID):
+	response = "This is the results of question %s."
+	return HttpResponse(response % qID)
+
+def vote(request, qID):
+	return HttpResponse("You want to vote %s question." % qID)
