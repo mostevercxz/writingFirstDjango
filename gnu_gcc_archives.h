e10c82a40e07d156fb29df7d0df7aa44be94f21c
purchaseDream <849069196@qq.com>
2015-08-07 22:41:59 +0800

    modify tips
    modify tips

--- gnu_gcc_archives.h
+++ gnu_gcc_archives.h
@@ -226,6 +226,31 @@ daemon(1,1)
 	
 	date, local time zone must be set--see zic manual page
 	cp /usr/share/zoneinfo/Asia/Shanghai /home/userName/bin/glibc/etc/localtime
+	
+11. gdb print	
+	print binary notion numbers,像十六进制那样,打印二进制的值(p 0b11111111)
+	p *array @ len
+	print/f <expr>  f代表输出的格式
+    x 按十六进制格式显示变量
+    d 按十进制格式显示变量
+    u 按十六进制格式显示无符号整型
+    o 按八进制格式显示变量
+    t 按二进制格式显示变量
+    a 按十六进制格式显示变量
+    c 按字符格式显示变量
+    f 按浮点数格式显示变量
+	
+	expressions表达式 @ :: {}
+	file::variable
+	function::variable
+	
+12. gdb 显示汇编
+	display/i $pc
+	undisplay
+	delete display
+	disable display
+	enable display
+	info display
 
 -------------------------------
 	gdb tricks I should know
@@ -413,3 +438,35 @@ struct epoll_event
 	__uint32_t events;
 	epoll_data_t data;
 };
+//represent binary number in C??
+#define B_0001  1
+#define B_0010  2
+#define B_0011  3
+#define B_0100  4
+#define B_0101  5
+#define B_0110  6
+#define B_0111  7
+#define B_1000  8
+#define B_1001  9
+#define B_1010  a
+#define B_1011  b
+#define B_1100  c
+#define B_1101  d
+#define B_1110  e
+#define B_1111  f
+
+#define _B2H(bits)      B_##bits
+#define B2H(bits)       _B2H(bits)
+#define _HEX(n)         0x##n##UL
+#define HEX(n)          _HEX(n)
+#define _CCAT(a,B)/>    a##b
+#define CCAT(a,B)/>     _CCAT(a,B)/>
+
+#define BYTE(a,B)/>                     HEX( CCAT(B2H( a),B2H( B)/>) )
+#define WORD(a,b,c,d)                   HEX( CCAT(CCAT(B2H( a),B2H( B)/>),CCAT(B2H( c),B2H( d))) )
+#define DWORD(a,b,c,d,e,f,g,h)  HEX(  CCAT( CCAT(CCAT(B2H( a),B2H( B)/>),CCAT(B2H( c),B2H( d))),CCAT(CCAT(B2H( e),B2H( f)),CCAT(B2H( g),B2H( h))) )  )
+
+        //using example
+        char b = BYTE(0100,0001); //equivalent to b = 65; or b = 'A'; or b = 0x41;
+        unsigned int w = WORD(1101,1111,0100,0011); //equivalent to w = 57155; or w = 0xdf43;
+        unsigned long int dw = DWORD(1101,1111,0100,0011,1111,1101,0010,1000); //equivalent to dw = 3745774888; or dw = 0xdf43fd28;
